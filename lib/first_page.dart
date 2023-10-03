c50bc78ce8136782ae0b6dbabe959a41d4fd9016
SK3620 <jiantail519@gmail.com>
2023-10-03 15:15:31 +0900

    超簡単なToDoアプリ完成

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'second_page.dart';
 
+class ToDoListPage extends StatefulWidget {
+@override
+ToDoListPageState createState() => ToDoListPageState();
+}
+
 // リスト一覧画面用Widget
-class ToDoListPage extends StatelessWidget {
-  String nameText = '';
+class ToDoListPageState extends State<ToDoListPage> {
+
+  List<String> toDoList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -13,38 +19,34 @@ class ToDoListPage extends StatelessWidget {
         backgroundColor: Colors.green,
         title: Text('リスト一覧'),
       ),
-      body: ListView(
-        children: <Widget> [
-          Card(
+      body: ListView.builder(
+        itemCount: this.toDoList.length,
+        // itemBuilderはType: Widget? Function(BuildContext, int)
+        itemBuilder: (context, index) {
+          return Card(
             child: ListTile(
-              title: Text('テスト1'),
+              title: Text(this.toDoList[index]),
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('テスト2'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('テスト3'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('テスト4'),
-            ),
-          ),
-        ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // "push"画面で新規画面に遷移
-          Navigator.of(context).push(
+        onPressed: () async {
+          // "push"で新規画面に遷移
+          // リスト追加画面から渡される値を受け取る
+          final newListText = await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
-              return SecondPage();
+              // 遷移先の画面としてリスト追加画面を指定
+              return ToDoAddPage();
             }),
           );
+          if (newListText != null) {
+            // キャンセルした場合は newListText が null となるので注意
+            setState(() {
+              // リスト追加
+              toDoList.add(newListText);
+            });
+          }
         },
         child: Icon(Icons.add),
       ),
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,28 +1,59 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatelessWidget {
+class ToDoAddPage extends StatefulWidget {
+  @override ToDoAddPageState createState() => ToDoAddPageState();
+}
+
+class ToDoAddPageState extends State<ToDoAddPage> {
+
+  String text = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: const Text('SecondPage'),
+        title: const Text('リスト追加'),
       ),
-      body: Center(
+      body: Container(
+        // 余白をつける
+        padding: EdgeInsets.all(64),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('こんにちは',
-              style: TextStyle(
-                fontSize: 50,
+          children: <Widget> [
+            TextField(
+              onChanged: (String value) {
+                // データがが変更されたことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  this.text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              //リストボタン追加
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: (){
+                  Navigator.of(context).pop(this.text);
+                  print(this.text);
+                },
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
               ),
             ),
-            ElevatedButton (
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('ボタン'),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                child: Text('キャンセル'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
             ),
           ],
         ),
